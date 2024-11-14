86ea9c8400c0cca21f18fd88e7e3104fb78348fa
Jimmyale3102 <jimmyale3102@gmail.com>
2024-11-14 12:29:55 +1100

    feat: Add Bottom Navigation routes

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/core/navigation/Routes.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/core/navigation/Routes.kt
@@ -2,4 +2,8 @@ package com.alejo.rickmortyapp.ui.core.navigation
 
 sealed class Routes(val route: String) {
     data object Home : Routes("home")
+
+    // Home Bottom Bar
+    data object Episodes : Routes("episodes")
+    data object Characters : Routes("characters")
 }
