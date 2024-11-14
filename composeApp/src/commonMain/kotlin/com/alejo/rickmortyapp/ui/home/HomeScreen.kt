f63e4565a0a230a62e086169faec05f18b0e4f71
Jimmyale3102 <jimmyale3102@gmail.com>
2024-11-14 12:29:36 +1100

    feat: Implement Bottom App Bar

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/home/HomeScreen.kt
@@ -1,9 +1,55 @@
 package com.alejo.rickmortyapp.ui.home
 
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.alejo.rickmortyapp.ui.core.navigation.bottomNavigation.BottomBarItem
+import com.alejo.rickmortyapp.ui.core.navigation.bottomNavigation.BottomBarItem.Characters
+import com.alejo.rickmortyapp.ui.core.navigation.bottomNavigation.BottomBarItem.Episodes
+import com.alejo.rickmortyapp.ui.core.navigation.bottomNavigation.BottomNavigationWrapper
 
 @Composable
 fun HomeScreen() {
-    Text("Heyyyyy Bro!")
+    val items = listOf(Episodes(), Characters())
+    val navController = rememberNavController()
+
+    Scaffold(
+        bottomBar = { BottomBar(items, navController) }
+    ) {
+        BottomNavigationWrapper(navController)
+    }
+}
+
+@Composable
+fun BottomBar(items: List<BottomBarItem>, navController: NavHostController) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+    BottomAppBar {
+        items.forEach { item ->
+            NavigationBarItem(
+                icon = item.icon,
+                label = { Text(item.title) },
+                selected = currentDestination?.hierarchy?.any { it.route == item.route } == true,
+                onClick = {
+                    navController.navigate(route = item.route) {
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+
+    }
 }
