403670fc0ee6fb0d2a9cfaffbf454f8cc809369f
Jimmyale3102 <jimmyale3102@gmail.com>
2024-11-18 14:14:53 +1100

    feat: Add Character response

---
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/data/remote/response/CharacterResponse.kt
@@ -0,0 +1,11 @@
+package com.alejo.rickmortyapp.data.remote.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CharacterResponse(
+    @SerialName("id") val id: String,
+    @SerialName("status") val status: String,
+    @SerialName("image") val image: String
+)
