f61d6301f27feff6100dc2972f690dd20e6331a0
Jimmyale3102 <jimmyale3102@gmail.com>
2024-11-18 14:15:27 +1100

    feat: Define the httpClient singleton

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/di/DataModule.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/di/DataModule.kt
@@ -1,7 +1,29 @@
 package com.alejo.rickmortyapp.di
 
+import com.alejo.rickmortyapp.data.remote.ApiService
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.http.ContentType
+import io.ktor.http.URLProtocol
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val dataModule = module {
-
+    single {
+        HttpClient {
+            install(ContentNegotiation) {
+                json( json = Json { ignoreUnknownKeys = true }, contentType = ContentType.Any )
+            }
+            install(DefaultRequest) {
+                url {
+                    protocol = URLProtocol.HTTPS
+                    host = "rickandmortyapi.com"
+                }
+            }
+        }
+    }
+    factoryOf(::ApiService)
 }
