105a8d3e7d36f57234be4686cb0008435cea1672
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-07 16:05:59 +1100

    feat: Inject RandomCharacter usecase

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/di/DomainModule.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/di/DomainModule.kt
@@ -1,7 +1,9 @@
 package com.alejo.rickmortyapp.di
 
+import com.alejo.rickmortyapp.domain.usecase.GetRandomCharacterUseCase
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val domainModule = module {
-
+    factoryOf(::GetRandomCharacterUseCase)
 }
