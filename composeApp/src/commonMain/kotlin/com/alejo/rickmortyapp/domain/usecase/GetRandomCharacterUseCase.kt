7ab6ae5b4b0a2b29c299f276a3f847257b5738c6
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-10 11:55:50 +1100

    feat: Return characterModel from usecase

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/domain/usecase/GetRandomCharacterUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/domain/usecase/GetRandomCharacterUseCase.kt
@@ -1,10 +1,11 @@
 package com.alejo.rickmortyapp.domain.usecase
 
 import com.alejo.rickmortyapp.domain.Repository
+import com.alejo.rickmortyapp.domain.model.CharacterModel
 
 class GetRandomCharacterUseCase(private val repository: Repository) {
-    suspend operator fun invoke() {
+    suspend operator fun invoke(): CharacterModel {
         val randomCharacterId: Int = (1..826).random()
-        repository.getCharacterById(randomCharacterId.toString())
+        return repository.getCharacterById(randomCharacterId.toString())
     }
 }
