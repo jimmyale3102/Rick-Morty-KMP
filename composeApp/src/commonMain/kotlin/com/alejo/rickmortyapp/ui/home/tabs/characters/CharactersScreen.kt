255cd5f288cb70cf73143fed962434972cd108b0
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-10 11:55:22 +1100

    feat: Connect Character screen to ViewModel

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/home/tabs/characters/CharactersScreen.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/home/tabs/characters/CharactersScreen.kt
@@ -1,13 +1,24 @@
 package com.alejo.rickmortyapp.ui.home.tabs.characters
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import org.koin.compose.viewmodel.koinViewModel
+import org.koin.core.annotation.KoinExperimentalAPI
 
+@OptIn(KoinExperimentalAPI::class)
 @Composable
 fun CharactersScreen() {
-    Box(Modifier.fillMaxSize()) {
+    val viewModel = koinViewModel<CharactersViewModel>()
+    val state by viewModel.state.collectAsState()
 
+    Column(Modifier.fillMaxSize()) {
+        state.characterOfTheDay?.let {
+            Text(it.image)
+        }
     }
 }
