39b1bcab4408890e9221dfb57bc0a1321915f4db
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-10 13:31:43 +1100

    feat: Create Preview from Android module

---
+++ composeApp/src/androidMain/kotlin/com/alejo/rickmortyapp/Preview.kt
@@ -0,0 +1,18 @@
+package com.alejo.rickmortyapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.alejo.rickmortyapp.domain.model.CharacterModel
+import com.alejo.rickmortyapp.ui.home.tabs.characters.CharacterOfTheDay
+
+@Preview
+@Composable
+fun ComponentPreview() {
+    CharacterOfTheDay(
+        CharacterModel(
+            id = "1",
+            isAlive = true,
+            image = "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+        )
+    )
+}
