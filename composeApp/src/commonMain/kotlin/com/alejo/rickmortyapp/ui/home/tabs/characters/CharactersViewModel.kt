ee3f377bd1c62d463dcf30c67f0892a262b9a77f
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-14 22:06:47 +1100

    feat: Link the characters viewmodel to the repository

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/home/tabs/characters/CharactersViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/home/tabs/characters/CharactersViewModel.kt
@@ -2,6 +2,7 @@ package com.alejo.rickmortyapp.ui.home.tabs.characters
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.alejo.rickmortyapp.domain.Repository
 import com.alejo.rickmortyapp.domain.usecase.GetRandomCharacterUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
@@ -12,7 +13,8 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class CharactersViewModel(
-    private val getRandomCharacterUseCase: GetRandomCharacterUseCase
+    private val getRandomCharacterUseCase: GetRandomCharacterUseCase,
+    private val repository: Repository
 ) : ViewModel() {
     private val _state = MutableStateFlow<CharacterState>(CharacterState())
     val state: StateFlow<CharacterState> = _state
@@ -24,5 +26,10 @@ class CharactersViewModel(
             }
             _state.update { state -> state.copy(characterOfTheDay = result) }
         }
+        getAllCharacters()
+    }
+
+    private fun getAllCharacters() {
+        _state.update { state -> state.copy(characters = repository.getAllCharacters()) }
     }
 }
