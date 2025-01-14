c279577b4d5abd6ac7a2743e63bd6171327bd45f
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-14 22:07:20 +1100

    feat: Add getAllCharacters function

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/domain/Repository.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/domain/Repository.kt
@@ -1,7 +1,10 @@
 package com.alejo.rickmortyapp.domain
 
+import androidx.paging.PagingData
 import com.alejo.rickmortyapp.domain.model.CharacterModel
+import kotlinx.coroutines.flow.Flow
 
 interface Repository {
     suspend fun getCharacterById(id: String): CharacterModel
+    fun getAllCharacters() : Flow<PagingData<CharacterModel>>
 }
