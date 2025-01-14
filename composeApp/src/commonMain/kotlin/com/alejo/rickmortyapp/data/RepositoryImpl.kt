b739945e426d8f76e31b064465c7dac87a99f754
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-14 22:07:27 +1100

    feat: Implement getAllCharacters function

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/data/RepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/data/RepositoryImpl.kt
@@ -1,11 +1,35 @@
 package com.alejo.rickmortyapp.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.alejo.rickmortyapp.data.remote.ApiService
+import com.alejo.rickmortyapp.data.remote.paging.CharactersPagingSource
 import com.alejo.rickmortyapp.domain.Repository
 import com.alejo.rickmortyapp.domain.model.CharacterModel
+import kotlinx.coroutines.flow.Flow
 
-class RepositoryImpl(private val api: ApiService) : Repository {
+class RepositoryImpl(
+    private val api: ApiService,
+    private val charactersPagingSource: CharactersPagingSource
+) : Repository {
+
+    companion object {
+        const val MAX_ITEMS = 20
+        const val PREFETCH_ITEMS = 5
+    }
+
     override suspend fun getCharacterById(id: String): CharacterModel {
         return api.getCharacterById(id).toDomain()
+    }
+
+    override fun getAllCharacters(page: Int): Flow<PagingData<CharacterModel>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = MAX_ITEMS,
+                prefetchDistance = PREFETCH_ITEMS
+            ),
+            pagingSourceFactory = { charactersPagingSource }
+        ).flow
     }
 }
