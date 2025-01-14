81340566ca31cfae8edae4e5038a9c3636e216fb
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-14 22:05:52 +1100

    feat: Add function to get all characters

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/data/remote/ApiService.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/data/remote/ApiService.kt
@@ -1,14 +1,22 @@
 package com.alejo.rickmortyapp.data.remote
 
 import com.alejo.rickmortyapp.data.remote.response.CharacterResponse
+import com.alejo.rickmortyapp.data.remote.response.CharactersWrapperResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 
 class ApiService(private val client: HttpClient) {
 
     suspend fun getCharacterById(id: String): CharacterResponse {
         return client.get("/api/character/$id").body()
+    }
+
+    suspend fun getAllCharacters(page: Int): CharactersWrapperResponse {
+        return client.get("/api/character/") {
+            parameter("page", page)
+        }.body()
     }
 
 }
