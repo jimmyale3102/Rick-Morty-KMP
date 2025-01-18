176f6b4e8b49dee3b2e1741cd1f1c6d06cf1fb94
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-19 00:33:48 +1100

    feat: Add Serializable annotation

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/data/remote/response/CharactersWrapperResponse.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/data/remote/response/CharactersWrapperResponse.kt
@@ -1,5 +1,8 @@
 package com.alejo.rickmortyapp.data.remote.response
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class CharactersWrapperResponse(
     val info: InfoResponse,
     val results: List<CharacterResponse>
