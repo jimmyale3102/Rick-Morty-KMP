29e75c9cbd9b049a4878426522531cd27a215c89
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-23 13:04:12 +1100

    feat: Fix vertical extension function

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/core/ext/ModifierExt.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/core/ext/ModifierExt.kt
@@ -7,8 +7,8 @@ fun Modifier.vertical() = layout { measurable, constraints ->
     val placeable = measurable.measure(constraints)
     layout(placeable.height, placeable.width) {
         placeable.place(
-            x = -((placeable.width / 2) - (placeable.height / 2)),
-            y = -((placeable.height / 2) - (placeable.width / 2))
+            x = -(placeable.width / 2 - placeable.height / 2),
+            y = -(placeable.height / 2 - placeable.width / 2)
         )
     }
 }
