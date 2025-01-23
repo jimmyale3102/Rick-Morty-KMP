32a892d40040aabc5d8fa8051c89105cb1d12fba
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-23 13:03:53 +1100

    feat: Set scaffold padding

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/home/HomeScreen.kt
@@ -1,11 +1,14 @@
 package com.alejo.rickmortyapp.ui.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -22,8 +25,10 @@ fun HomeScreen() {
 
     Scaffold(
         bottomBar = { BottomBar(items, navController) }
-    ) {
-        BottomNavigationWrapper(navController)
+    ) { padding ->
+        Box(modifier = Modifier.padding(padding)) {
+            BottomNavigationWrapper(navController)
+        }
     }
 }
 
