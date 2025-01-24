986e67fefd7e982051c31a9b1e1d0827571bcec6
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-25 10:26:11 +1100

    feat: Update RepositoryImpl Injection

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/di/DataModule.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/di/DataModule.kt
@@ -29,6 +29,6 @@ val dataModule = module {
         }
     }
     factoryOf(::ApiService)
-    factory <Repository>{ RepositoryImpl(get(), get()) }
+    factory <Repository>{ RepositoryImpl(get(), get(), get()) }
     factoryOf(::CharactersPagingSource)
 }
