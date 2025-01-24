ac02dd4c682e5a04e9d9e0aca8347423d0a08e9d
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-25 10:27:15 +1100

    feat: Add abstract dao function

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/data/database/RickMortyDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/data/database/RickMortyDatabase.kt
@@ -4,6 +4,7 @@ import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.RoomDatabaseConstructor
+import com.alejo.rickmortyapp.data.database.dao.UserPreferencesDAO
 import com.alejo.rickmortyapp.data.database.entity.CharacterOfTheDayEntity
 
 const val DATABASE_NAME = "rick_morty_database.db"
@@ -13,5 +14,5 @@ expect object RickMortyCTor: RoomDatabaseConstructor<RickMortyDatabase>
 @Database(entities = [CharacterOfTheDayEntity::class], version = 1)
 @ConstructedBy(RickMortyCTor::class)
 abstract class RickMortyDatabase : RoomDatabase() {
-    // DAO
+    abstract fun getPreferencesDAO(): UserPreferencesDAO
 }
