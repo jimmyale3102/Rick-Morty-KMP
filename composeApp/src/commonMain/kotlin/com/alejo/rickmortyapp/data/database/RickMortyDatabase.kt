2f0ec90f81a1a52d5df8ca56afa77fd7d82c5c88
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-24 21:20:25 +1100

    feat: Add Database class

---
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/data/database/RickMortyDatabase.kt
@@ -0,0 +1,17 @@
+package com.alejo.rickmortyapp.data.database
+
+import androidx.room.ConstructedBy
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
+import com.alejo.rickmortyapp.data.database.entity.CharacterOfTheDayEntity
+
+const val DATABASE_NAME = "rick_morty_database.db"
+
+expect object RickMortyCTor: RoomDatabaseConstructor<RickMortyDatabase>
+
+@Database(entities = [CharacterOfTheDayEntity::class], version = 1)
+@ConstructedBy(RickMortyCTor::class)
+abstract class RickMortyDatabase : RoomDatabase() {
+    // DAO
+}
