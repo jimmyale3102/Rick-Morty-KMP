8acc23348299cd89c6b18a7dfab557a34f03a0b2
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-25 10:25:17 +1100

    feat: Build database for android

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/domain/usecase/GetRandomCharacterUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/domain/usecase/GetRandomCharacterUseCase.kt
@@ -2,10 +2,33 @@ package com.alejo.rickmortyapp.domain.usecase
 
 import com.alejo.rickmortyapp.domain.Repository
 import com.alejo.rickmortyapp.domain.model.CharacterModel
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 class GetRandomCharacterUseCase(private val repository: Repository) {
     suspend operator fun invoke(): CharacterModel {
+
+        /*
+        val characterSaved = repository.savedCharacter()
+        if (characterSaved != null) {
+            if (characterSaved.date == getCurrentDayOfTheYear()) {
+                return characterSaved
+            }
+        } else {
+
+
+        }
+         */
+        repository.getCharacterOfTheDayDB()
         val randomCharacterId: Int = (1..826).random()
         return repository.getCharacterById(randomCharacterId.toString())
+    }
+
+    private fun getCurrentDayOfTheYear(): String {
+        val instant: Instant = Clock.System.now()
+        val localDateTime = instant.toLocalDateTime(TimeZone.currentSystemDefault())
+        return "${localDateTime.dayOfYear}${localDateTime.year}"
     }
 }
