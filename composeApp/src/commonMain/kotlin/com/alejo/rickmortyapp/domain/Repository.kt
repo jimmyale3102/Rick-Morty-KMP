47c11e894c5f4ba5d0a62e81bf1318ee21683601
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-25 12:06:37 +1100

    feat: Get and Save character of the day

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/domain/Repository.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/domain/Repository.kt
@@ -2,10 +2,12 @@ package com.alejo.rickmortyapp.domain
 
 import androidx.paging.PagingData
 import com.alejo.rickmortyapp.domain.model.CharacterModel
+import com.alejo.rickmortyapp.domain.model.CharacterOfTheDayModel
 import kotlinx.coroutines.flow.Flow
 
 interface Repository {
     suspend fun getCharacterById(id: String): CharacterModel
     fun getAllCharacters() : Flow<PagingData<CharacterModel>>
-    suspend fun getCharacterOfTheDayDB()
+    suspend fun getCharacterOfTheDayDB() : CharacterOfTheDayModel?
+    suspend fun saveCharacterEntity(characterOfTheDayModel: CharacterOfTheDayModel)
 }
