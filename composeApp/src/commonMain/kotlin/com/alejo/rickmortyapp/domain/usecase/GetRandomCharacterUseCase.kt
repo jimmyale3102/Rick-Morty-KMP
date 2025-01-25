4b37d7e1d5275b1ecbcec86c046174cf92e7a070
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-25 12:06:17 +1100

    feat: Update random use case to persist the data

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/domain/usecase/GetRandomCharacterUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/domain/usecase/GetRandomCharacterUseCase.kt
@@ -2,6 +2,7 @@ package com.alejo.rickmortyapp.domain.usecase
 
 import com.alejo.rickmortyapp.domain.Repository
 import com.alejo.rickmortyapp.domain.model.CharacterModel
+import com.alejo.rickmortyapp.domain.model.CharacterOfTheDayModel
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
@@ -9,19 +10,23 @@ import kotlinx.datetime.toLocalDateTime
 
 class GetRandomCharacterUseCase(private val repository: Repository) {
     suspend operator fun invoke(): CharacterModel {
-
-        /*
-        val characterSaved = repository.savedCharacter()
-        if (characterSaved != null) {
-            if (characterSaved.date == getCurrentDayOfTheYear()) {
-                return characterSaved
-            }
+        val characterEntity = repository.getCharacterOfTheDayDB()
+        val currentDay = getCurrentDayOfTheYear()
+        return if (characterEntity != null && characterEntity.selectedDay == currentDay) {
+            characterEntity.character
         } else {
-
-
+            val character = getRandomCharacter(repository)
+            repository.saveCharacterEntity(
+                CharacterOfTheDayModel(
+                    character = character,
+                    selectedDay = currentDay
+                )
+            )
+            character
         }
-         */
-        repository.getCharacterOfTheDayDB()
+    }
+
+    private suspend fun getRandomCharacter(repository: Repository): CharacterModel {
         val randomCharacterId: Int = (1..826).random()
         return repository.getCharacterById(randomCharacterId.toString())
     }
