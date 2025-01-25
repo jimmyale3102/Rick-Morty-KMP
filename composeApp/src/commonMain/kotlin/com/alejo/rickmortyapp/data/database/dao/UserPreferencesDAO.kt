50171da2d25938ab16d5dc5340e82c724a20c5ec
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-25 12:07:05 +1100

    feat: Insert character of the day into database

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/data/database/dao/UserPreferencesDAO.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/data/database/dao/UserPreferencesDAO.kt
@@ -1,6 +1,8 @@
 package com.alejo.rickmortyapp.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.alejo.rickmortyapp.data.database.entity.CharacterOfTheDayEntity
 
@@ -9,5 +11,8 @@ interface UserPreferencesDAO {
 
     @Query("SELECT * FROM character_of_the_day")
     suspend fun getCharacterOfTheDay(): CharacterOfTheDayEntity?
+
+    @Insert(entity = CharacterOfTheDayEntity::class, onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveCharacterOfTheDay(characterOfTheDayEntity: CharacterOfTheDayEntity)
 
 }
