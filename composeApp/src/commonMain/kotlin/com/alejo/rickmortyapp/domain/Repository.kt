1a8586bf175a996e0a81ead1c9d245077f8de93c
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-27 18:46:08 +1100

    feat: Add function to get all episodes in the repository

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/domain/Repository.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/domain/Repository.kt
@@ -3,6 +3,7 @@ package com.alejo.rickmortyapp.domain
 import androidx.paging.PagingData
 import com.alejo.rickmortyapp.domain.model.CharacterModel
 import com.alejo.rickmortyapp.domain.model.CharacterOfTheDayModel
+import com.alejo.rickmortyapp.domain.model.EpisodeModel
 import kotlinx.coroutines.flow.Flow
 
 interface Repository {
@@ -10,4 +11,5 @@ interface Repository {
     fun getAllCharacters() : Flow<PagingData<CharacterModel>>
     suspend fun getCharacterOfTheDayDB() : CharacterOfTheDayModel?
     suspend fun saveCharacterEntity(characterOfTheDayModel: CharacterOfTheDayModel)
+    fun getAllEpisodes() : Flow<PagingData<EpisodeModel>>
 }
