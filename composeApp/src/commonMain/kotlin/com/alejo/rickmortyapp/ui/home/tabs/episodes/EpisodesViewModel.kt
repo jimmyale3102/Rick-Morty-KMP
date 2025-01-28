274ea989b26745b186c1419785e23800edb095d7
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-28 11:34:00 +1100

    feat: Get Episodes from ViewModel

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/home/tabs/episodes/EpisodesViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/home/tabs/episodes/EpisodesViewModel.kt
@@ -1,9 +1,22 @@
 package com.alejo.rickmortyapp.ui.home.tabs.episodes
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.alejo.rickmortyapp.domain.Repository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
-class EpisodesViewModel : ViewModel() {
+class EpisodesViewModel(private val repository: Repository) : ViewModel() {
+    private val _state = MutableStateFlow<EpisodesState>(EpisodesState())
+    val state: StateFlow<EpisodesState> = _state
 
-
-
+    init {
+        _state.update { episodesState ->
+            episodesState.copy(
+                episodes = repository.getAllEpisodes().cachedIn(viewModelScope)
+            )
+        }
+    }
 }
