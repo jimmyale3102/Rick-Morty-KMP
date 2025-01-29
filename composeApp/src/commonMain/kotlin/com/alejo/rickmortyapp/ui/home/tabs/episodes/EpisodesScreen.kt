bd05b3b3762da5976e3bf3d920a5581f3c708e82
Jimmyale3102 <jimmyale3102@gmail.com>
2025-01-30 10:50:46 +1100

    feat: Create videoPlayer composable

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/home/tabs/episodes/EpisodesScreen.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/home/tabs/episodes/EpisodesScreen.kt
@@ -1,28 +1,49 @@
 package com.alejo.rickmortyapp.ui.home.tabs.episodes
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import app.cash.paging.compose.collectAsLazyPagingItems
 import com.alejo.rickmortyapp.domain.model.EpisodeModel
 import com.alejo.rickmortyapp.domain.model.SeasonEpisode
-import com.alejo.rickmortyapp.domain.model.SeasonEpisode.*
+import com.alejo.rickmortyapp.domain.model.SeasonEpisode.SEASON_1
+import com.alejo.rickmortyapp.domain.model.SeasonEpisode.SEASON_2
+import com.alejo.rickmortyapp.domain.model.SeasonEpisode.SEASON_3
+import com.alejo.rickmortyapp.domain.model.SeasonEpisode.SEASON_4
+import com.alejo.rickmortyapp.domain.model.SeasonEpisode.SEASON_5
+import com.alejo.rickmortyapp.domain.model.SeasonEpisode.SEASON_6
+import com.alejo.rickmortyapp.domain.model.SeasonEpisode.SEASON_7
+import com.alejo.rickmortyapp.domain.model.SeasonEpisode.UNKNOWN
 import com.alejo.rickmortyapp.ui.core.composables.PagingType
 import com.alejo.rickmortyapp.ui.core.composables.PagingWrapper
 import com.alejo.rickmortyapp.ui.core.composables.RickMortyLoading
+import com.alejo.rickmortyapp.ui.core.composables.VideoPlayer
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.viewmodel.koinViewModel
@@ -44,32 +65,66 @@ fun EpisodesScreen() {
     val state by viewModel.state.collectAsState()
     val episodes = state.episodes.collectAsLazyPagingItems()
 
-    Box(Modifier.fillMaxSize()) {
+    Column(Modifier.fillMaxSize().padding(top = 16.dp)) {
         PagingWrapper(
             pagingType = PagingType.ROW,
             pagingItems = episodes,
             initView = { RickMortyLoading() },
             loadingItemsView = { RickMortyLoading() },
         ) { episode ->
-            EpisodeItem(episode)
+            EpisodeItem(episode) { videoUrl ->
+                viewModel.onVideoSelected(videoUrl)
+            }
+        }
+        AnimatedVisibility (state.playVideo.isNotBlank()) {
+            VideoComponent(state.playVideo) { viewModel.onVideoClosed() }
         }
     }
 }
 
 @Composable
-fun EpisodeItem(episode: EpisodeModel) {
+private fun VideoComponent(videoUrl: String, onVideoClosed: () -> Unit) {
+    ElevatedCard(
+        modifier = Modifier.fillMaxWidth()
+            .padding(16.dp)
+            .height(250.dp)
+            .border(2.dp, Color.Green, CardDefaults.elevatedShape)
+    ) {
+        Box(modifier = Modifier.fillMaxSize().background(Color.Black)) {
+            Box(modifier = Modifier.padding(16.dp), contentAlignment = Alignment.Center) {
+                VideoPlayer(
+                    modifier = Modifier.fillMaxWidth(),
+                    url = videoUrl
+                )
+            }
+            Row(Modifier.fillMaxWidth().padding(8.dp)) {
+                Spacer(modifier = Modifier.weight(1f))
+                Image(
+                    modifier = Modifier.clickable { onVideoClosed() },
+                    imageVector = Icons.Default.Close,
+                    contentDescription = null,
+                    colorFilter = ColorFilter.tint(Color.White)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun EpisodeItem(episode: EpisodeModel, onEpisodeSelected: (String) -> Unit) {
     Box(
         modifier = Modifier
-            .width(120.dp)
+            .width(160.dp)
             .padding(horizontal = 4.dp)
-            .clickable {  },
+            .clickable { onEpisodeSelected(episode.videoURL) }
+            .clip(RoundedCornerShape(8)),
         contentAlignment = Alignment.Center
     ) {
         Image(
             modifier = Modifier.fillMaxWidth(),
             painter = painterResource(getSeasonImage(episode.season)),
             contentDescription = episode.name,
-            contentScale = ContentScale.Inside
+            contentScale = ContentScale.Crop
         )
     }
 }
