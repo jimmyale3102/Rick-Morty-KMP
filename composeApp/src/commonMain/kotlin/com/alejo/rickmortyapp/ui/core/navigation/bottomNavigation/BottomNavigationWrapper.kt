ff608572ace8165b759db71fb4f0d134a49e04a9
Jimmyale3102 <jimmyale3102@gmail.com>
2025-02-01 09:33:21 +1100

    feat: Add navigation to Character Details Screen

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/core/navigation/bottomNavigation/BottomNavigationWrapper.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/core/navigation/bottomNavigation/BottomNavigationWrapper.kt
@@ -4,18 +4,26 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.alejo.rickmortyapp.ui.core.navigation.CharacterDetails
 import com.alejo.rickmortyapp.ui.core.navigation.Routes
 import com.alejo.rickmortyapp.ui.home.tabs.characters.CharactersScreen
 import com.alejo.rickmortyapp.ui.home.tabs.episodes.EpisodesScreen
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 @Composable
-fun BottomNavigationWrapper(navController: NavHostController) {
+fun BottomNavigationWrapper(navController: NavHostController, mainNavController: NavHostController) {
     NavHost(navController = navController, startDestination = Routes.Episodes.route) {
         composable(Routes.Episodes.route) {
             EpisodesScreen()
         }
         composable(Routes.Characters.route) {
-            CharactersScreen()
+            CharactersScreen(
+                navigateToCharacterDetails = { character ->
+                    val characterEncoded = Json.encodeToString(character)
+                    mainNavController.navigate(CharacterDetails(characterEncoded))
+                }
+            )
         }
     }
 }
