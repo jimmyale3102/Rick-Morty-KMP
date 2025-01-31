1017c40550986ce61ec169237568b80ad1fcad6a
Jimmyale3102 <jimmyale3102@gmail.com>
2025-02-01 09:35:25 +1100

    feat: Add CharacterDetailsScreen to the routes

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/core/navigation/Routes.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/core/navigation/Routes.kt
@@ -1,5 +1,7 @@
 package com.alejo.rickmortyapp.ui.core.navigation
 
+import kotlinx.serialization.Serializable
+
 sealed class Routes(val route: String) {
     data object Home : Routes("home")
 
@@ -7,3 +9,6 @@ sealed class Routes(val route: String) {
     data object Episodes : Routes("episodes")
     data object Characters : Routes("characters")
 }
+
+@Serializable
+data class CharacterDetails(val characterModel: String)
