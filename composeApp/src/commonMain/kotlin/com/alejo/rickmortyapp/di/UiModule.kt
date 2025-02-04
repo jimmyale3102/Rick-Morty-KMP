792537dac08a97098d9f4da858125a2d2a3f7edc
Jimmyale3102 <jimmyale3102@gmail.com>
2025-02-04 11:10:34 +1100

    feat: Inject ViewModel

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/di/UiModule.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/di/UiModule.kt
@@ -1,5 +1,6 @@
 package com.alejo.rickmortyapp.di
 
+import com.alejo.rickmortyapp.ui.character.CharacterDetailsViewModel
 import com.alejo.rickmortyapp.ui.home.tabs.characters.CharactersViewModel
 import com.alejo.rickmortyapp.ui.home.tabs.episodes.EpisodesViewModel
 import org.koin.compose.viewmodel.dsl.viewModelOf
@@ -8,4 +9,5 @@ import org.koin.dsl.module
 val uiModule = module {
     viewModelOf(::CharactersViewModel)
     viewModelOf(::EpisodesViewModel)
+    viewModelOf(::CharacterDetailsViewModel)
 }
