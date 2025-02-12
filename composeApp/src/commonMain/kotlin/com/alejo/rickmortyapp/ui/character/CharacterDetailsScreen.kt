4b93cf281d0f6fe019c6c5e41590a87d59e4a238
Jimmyale3102 <jimmyale3102@gmail.com>
2025-02-13 09:04:32 +1100

    feat: Set colors to Details Screen

--- composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/character/CharacterDetailsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/alejo/rickmortyapp/ui/character/CharacterDetailsScreen.kt
@@ -5,16 +5,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
@@ -27,7 +30,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Black
-import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.layout.ContentScale
@@ -37,6 +39,12 @@ import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.alejo.rickmortyapp.domain.model.CharacterModel
 import com.alejo.rickmortyapp.domain.model.EpisodeModel
+import com.alejo.rickmortyapp.ui.core.BackgroundPrimaryColor
+import com.alejo.rickmortyapp.ui.core.BackgroundSecondaryColor
+import com.alejo.rickmortyapp.ui.core.BackgroundTertiaryColor
+import com.alejo.rickmortyapp.ui.core.DefaultTextColor
+import com.alejo.rickmortyapp.ui.core.Green
+import com.alejo.rickmortyapp.ui.core.Pink
 import com.alejo.rickmortyapp.ui.core.ext.aliveBorder
 import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.viewmodel.koinViewModel
@@ -52,33 +60,62 @@ fun CharacterDetailsScreen(character: CharacterModel) {
     val state by viewModel.uiState.collectAsState()
     val scrollState = rememberScrollState()
 
-    Column(Modifier.fillMaxSize().background(White).verticalScroll(scrollState)) {
+    Column(Modifier.fillMaxSize().background(BackgroundPrimaryColor).verticalScroll(scrollState)) {
         MainHeader(state.character)
-        CharacterInformation(state.character)
-        CharacterEpisodes(state.episodes)
-    }
-}
-
-@Composable
-fun CharacterInformation(character: CharacterModel) {
-    ElevatedCard(modifier = Modifier.padding(16.dp).fillMaxWidth()) {
-        Column() {
-            Text(character.name)
-            Text(character.gender)
+        Spacer(Modifier.height(16.dp))
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .clip(RoundedCornerShape(topStartPercent = 6, topEndPercent = 6))
+                .background(BackgroundSecondaryColor)
+        ) {
+            CharacterInformation(state.character)
+            CharacterEpisodes(state.episodes)
         }
     }
 }
 
 @Composable
+fun CharacterInformation(character: CharacterModel) {
+    ElevatedCard(
+        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+        colors = CardDefaults.elevatedCardColors(containerColor = BackgroundTertiaryColor)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text("About the character", color = DefaultTextColor)
+            Spacer(Modifier.height(4.dp))
+            InformationDetails(title = "Origin: ", detail = character.origin)
+            Spacer(Modifier.height(2.dp))
+            InformationDetails(title = "Gender: ", detail = character.gender)
+        }
+    }
+}
+
+@Composable
+fun InformationDetails(title: String, detail: String) {
+    Row {
+        Text(text = title, color = Black, fontWeight = FontWeight.Bold)
+        Spacer(Modifier.width(4.dp))
+        Text(text = detail, color = Green)
+    }
+}
+
+@Composable
 fun CharacterEpisodes(episodes: List<EpisodeModel>?) {
-    ElevatedCard(modifier = Modifier.padding(16.dp).fillMaxWidth()) {
-        Box(contentAlignment = Alignment.Center) {
+    ElevatedCard(
+        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+        colors = CardDefaults.elevatedCardColors(containerColor = BackgroundTertiaryColor)
+    ) {
+        Box(modifier = Modifier.padding(16.dp), contentAlignment = Alignment.Center) {
             if (episodes == null) {
-                CircularProgressIndicator(color = Color.Green)
+                CircularProgressIndicator(color = Green)
             } else {
                 Column(Modifier.fillMaxWidth()) {
+                    Text("Character episodes", color = DefaultTextColor)
+                    Spacer(Modifier.height(6.dp))
+
                     episodes.forEach { episode ->
                         EpisodeItem(episode)
+                        Spacer(Modifier.height(4.dp))
                     }
                 }
             }
@@ -88,8 +125,8 @@ fun CharacterEpisodes(episodes: List<EpisodeModel>?) {
 
 @Composable
 fun EpisodeItem(episode: EpisodeModel) {
-    Text(episode.name)
-    Text(episode.episode)
+    Text(episode.name, color = Green, fontWeight = FontWeight.Bold)
+    Text(episode.episode, color = DefaultTextColor)
 }
 
 @Composable
@@ -119,7 +156,7 @@ private fun CharacterHeader(character: CharacterModel) {
         ) {
             Text(
                 text = character.name,
-                color = Black,
+                color = Pink,
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold
             )
